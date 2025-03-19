a5b96fd3157130176ddd26cbee48452fdabee819
ashraf <sadarsign@gmail.com>
2025-03-19 17:41:24 +0300

    Initialized

--- script.js
+++ script.js
@@ -16,7 +16,11 @@ diceEl.classList.add('hidden');
 // Rolling dice functionality
 btnRoll.addEventListener('click', () => {
   // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
   // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
   // 3. Check for rolled 1
 });
 
