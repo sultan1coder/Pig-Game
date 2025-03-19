cbf4a88b1497faa2cfc1035ac52b689c20d49404
ashraf <sadarsign@gmail.com>
2025-03-19 17:51:59 +0300

    Initialized

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +14,8 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -24,10 +28,10 @@ btnRoll.addEventListener('click', () => {
   // 3. Check for rolled 1
   if (dice !== 1) {
     // Add dice to current score
-    
+    currentScore += dice;
+    current0El.textContent = currentScore;
   } else {
     // Switch to next player
-
   }
 });
 
